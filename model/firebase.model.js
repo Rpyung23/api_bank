39b1fccd0de4db4063de2b10215643c0a9f204ba
Rpyung23 <guaman1579@gmail.com>
2024-05-09 19:16:48 -0500

    update

--- model/firebase.model.js
+++ model/firebase.model.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
+//GOOGLE_APPLICATION_CREDENTIALS = .\util\firebase.json
 const {initializeApp,applicationDefault} = require('firebase-admin/app');
 const {getMessaging} = require("firebase-admin/messaging");
 const {getDatabase} = require("firebase-admin/database");
 
 initializeApp({
     credential: applicationDefault(),
+    privateKey:'./util/firebase.json',
     databaseURL: 'https://bank-da882-default-rtdb.firebaseio.com/',
     algorithm: 'RS256'
 });
